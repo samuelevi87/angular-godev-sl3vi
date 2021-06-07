ee6a2f83b9a94e6624e540c400cb92aa5664c1bd
samuelevi87 <samuel.levi.alves@outlook.com>
2021-06-07 17:49:28 -0300

    Project, Editor and Display list created

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 0o1, name: 'Dr Levi' },
+  { id: 0o2, name: 'NarCoca' },
+  { id: 0o03, name: 'MultiDinamite' },
+  { id: 4, name: 'Taca-lhe Pau' },
+  { id: 5, name: 'Imantado' },
+  { id: 6, name: 'BorrachaBoy' },
+  { id: 7, name: 'EletroChoque' },
+  { id: 8, name: 'Dr Noob' },
+  { id: 9, name: 'GomuGomu no Mi' },
+  { id: 10, name: 'Tornado' }
+];
