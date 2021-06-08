20018eeed7b5102190ce5cf433026c456a2e73dd
samuelevi87 <samuel.levi.alves@outlook.com>
2021-06-07 21:36:20 -0300

    added a hero detail component

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,12 +1,12 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 0o1, name: 'Dr Levi' },
-  { id: 0o2, name: 'NarCoca' },
-  { id: 0o03, name: 'MultiDinamite' },
+  { id: 1, name: 'Dr Levi' },
+  { id: 2, name: 'NarCoca' },
+  { id: 3, name: 'MultiDinamite' },
   { id: 4, name: 'Taca-lhe Pau' },
   { id: 5, name: 'Imantado' },
-  { id: 6, name: 'BorrachaBoy' },
+  { id: 6, name: 'StashBoy' },
   { id: 7, name: 'EletroChoque' },
   { id: 8, name: 'Dr Noob' },
   { id: 9, name: 'GomuGomu no Mi' },
